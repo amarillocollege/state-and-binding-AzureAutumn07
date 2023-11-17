7c1c4f2877732e160251f0454b289fbec136ea70
Samantha Castillo <smcastillo22@amarillocollege.com>
2023-11-17 00:45:25 -0600

    Final State&Binding

--- State and Binding/ContentView.swift
+++ State and Binding/ContentView.swift
@@ -2,30 +2,53 @@
 //  ContentView.swift
 //  State and Binding
 //
-//  Created by [YOUR NAME GOES HERE]
+//  Created by Samantha Castillo
 //
 
 import SwiftUI
 
 
 struct ContentView: View {
-    var counter = 1
+    @State var counter1 = 1
+    @State var counter2 = 1
+    @State var counter3 = 1
     
     var body: some View {
         VStack {
+            Spacer()
+            Text("\(counter1 + counter2 + counter3)")
+                .font(.system(size: 100, weight: .bold, design: .rounded))
+                .foregroundStyle(.black)
+            Spacer()
+            HStack {
+                CounterButton(counter: $counter1, color: .red)
+                CounterButton(counter: $counter2, color: .blue)
+                CounterButton(counter: $counter3, color: .green)
+                
+            }
+            
+        }
+    }
+}
+
+struct CounterButton: View {
+    @Binding var counter: Int
+    var color: Color
+    
+    var body: some View {
+        HStack {
             Button {
                 counter += 1
             } label: {
                 Circle()
-                    .frame(width: 200, height: 200)
-                    .foregroundStyle(.red)
-                    .overlay {
+                    .frame(width: 120, height: 120)
+                    .foregroundStyle(color)
+                    .overlay(
                         Text("\(counter)")
-                            .font(.system(size: 100, weight: .bold, design: .rounded))
+                            .font(.system(size: 60, weight: .bold, design: .rounded))
                             .foregroundStyle(.white)
-                    }
+                    )
             }
-            
         }
     }
 }
